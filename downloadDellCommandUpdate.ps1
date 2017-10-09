c14fcda7d98921607d987f1e40e332c461baf956
Brandon Kurtz <brandon.kurtz@bk8262139mbp.local>
2017-10-09 18:15:39 -0400

    contribution by Gyrferret

--- downloadDellCommandUpdate.ps1
+++ downloadDellCommandUpdate.ps1
@@ -1,25 +1,9 @@
-# (1) Disables firstrunCustomize for Internet Explorer
-# (to prevent failure if IE hasn't ever been launched)
-# ------------------------
-$registryPath = "HKCU:\Software\Policies\Microsoft\Internet Explorer\Main\"
-$Name = "DisablefirstrunCustomize"
-$value = 1
-# -force is needed in case the registry path doesn't exist
-New-Item -Path $registryPath -Name $name -Value $value -force
+$base_url = "http://www.dell.com/support/home/us/en/19/Drivers/DriversDetails?driverId=FXD2R"
+$version_history = (Invoke-WebRequest -Uri $base_url )
+$Latest = ($version_history.links | Where innerText -like "*.*.*,A**")[0]
 
+$latest
 
-
-
-
-# (2) Downloads latest Dell Command | Update
-# ------------------------
-# This link leads to a non-current version of Dell Command | Update. This way
-#   we can grab a direct link to the latest version under "Other Versions"
-#   and be future proof.
-$oldDellCommandUpdate = "http://www.dell.com/support/home/us/en/19/Drivers/DriversDetails?driverId=FXD2R"
-
-
-$version_list = (Invoke-WebRequest -Uri $base_url ) | Where innerText -like "*.*.*,A**"
-#$latest_executable = $links | Where outerText -eq "Other Versions" | Select-Object
-#$specific_url = $links | Where outerText -eq "Download File" | Select-Object -Expand href
-#Invoke-WebRequest -Uri $specific_url -outFile "dellCommandUpdate.exe"
+$Latest_URL = "http://www.dell.com" + $Latest.href
+$latest_executable = (Invoke-WebRequest $Latest_URL).links | Where outerText -eq "Download File"
+Start-BitsTransfer -source $latest_executable.href -destination "."
