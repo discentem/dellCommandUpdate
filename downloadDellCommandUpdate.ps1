231eb74a1782fdb878165da2078e59f64996d150
Brandon Kurtz <brandon.kurtz@bk8262139mbp.local>
2017-10-18 10:54:21 -0400

    added IE first run registry key setting back

--- downloadDellCommandUpdate.ps1
+++ downloadDellCommandUpdate.ps1
@@ -1,7 +1,15 @@
+$registryPath = "HKCU:\Software\Policies\Microsoft\Internet Explorer\Main\"
+$Name = "DisablefirstrunCustomize"
+$value = 1
+# -force is needed in case the registry path doesn't exist
+New-Item -Path $registryPath -Name $name -Value $value -force
+
+
+
 $base_url = "http://www.dell.com/support/home/us/en/19/Drivers/DriversDetails?driverId=FXD2R"
 $version_history = (Invoke-WebRequest -Uri $base_url )
 $Latest = ($version_history.links | Where innerText -like "*.*.*,A**")[0]
 
-$Latest_URL = "http://www.dell.com" + $Latest.href
+$Latest_URL = "http://www.dell.com"  $Latest.href
 $latest_executable = (Invoke-WebRequest $Latest_URL).links | Where outerText -eq "Download File"
 Start-BitsTransfer -source $latest_executable.href -destination "."
