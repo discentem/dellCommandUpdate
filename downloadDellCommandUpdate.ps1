a816ad8875b78d70c7497f83bf014957da5d525d
Brandon Kurtz <brandon.kurtz@bk8262139mbp.local>
2017-10-18 11:29:34 -0400

    added missing + symbol

--- downloadDellCommandUpdate.ps1
+++ downloadDellCommandUpdate.ps1
@@ -10,6 +10,6 @@ $base_url = "http://www.dell.com/support/home/us/en/19/Drivers/DriversDetails?dr
 $version_history = (Invoke-WebRequest -Uri $base_url )
 $Latest = ($version_history.links | Where innerText -like "*.*.*,A**")[0]
 
-$Latest_URL = "http://www.dell.com"  $Latest.href
+$Latest_URL = "http://www.dell.com" + $Latest.href
 $latest_executable = (Invoke-WebRequest $Latest_URL).links | Where outerText -eq "Download File"
 Start-BitsTransfer -source $latest_executable.href -destination "."
