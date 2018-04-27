99451a52c406508c661ab2c19f06031c238497b1
brandon.kurtz <brandon.kurtz@iacapps.com>
2018-04-26 21:37:34 -0400

    updating for py3

--- downloadDellCommandUpdate.py
+++ downloadDellCommandUpdate.py
@@ -1,4 +1,4 @@
-import urllib2
+import urllib.request
 import pprint
 from collections import OrderedDict
 from bs4 import BeautifulSoup
@@ -6,8 +6,8 @@ from bs4 import BeautifulSoup
 base_url = "http://www.dell.com/support/home/us/en/19/Drivers/DriversDetails?driverId=FXD2R"
 
 hdr = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'}
-request = urllib2.Request(base_url, headers=hdr)
-resp = urllib2.urlopen(request)
+request = urllib.request.Request(base_url, headers=hdr)
+resp = urllib.request.urlopen(request)
 
 content = BeautifulSoup(resp.read(), 'lxml')
 links = content.find_all('a', href=True)
@@ -27,4 +27,7 @@ for link in links:
         text = link.text.strip()
         other_versions[text] = "www.dell.com{0}".format(link['href'])
 
-latest_dcu = other_versions.items()[0]
+for key, val in other_versions.items():
+    print(key, val)
+latest_dcu = list(other_versions)[0]
+print(latest_dcu)
